6be23592b80b0d8bbdbca9dba9f33499ca441fbe
Ederson Silva <eder.ederson.cc@gmail.com>
2019-07-27 17:14:53 -0300

    Seeder tb_autor

---
+++ database/seeds/TbAutorTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class TbAutorTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	/*
+        DB::table('tb_autor')->insert([
+        	'desc_nome' => 'Bernard Conrwell'
+        ]);
+
+        DB::table('tb_autor')->insert([
+        	'desc_nome' => 'Fiódor Dostoiévski'
+        ]);
+
+        DB::table('tb_autor')->insert([
+        	'desc_nome' => 'Olavo de Carvalho'
+        ]);
+
+        DB::table('tb_autor')->insert([
+        	'desc_nome' => 'George R. R. Martin'
+        ]);
+
+        DB::table('tb_autor')->insert([
+        	'desc_nome' => 'Fiódor Dostoiévski'
+        ]);
+
+        DB::table('tb_autor')->insert([
+        	'desc_nome' => 'John R. R. Tolkien'
+        ]);
+        */
+    }
+}
